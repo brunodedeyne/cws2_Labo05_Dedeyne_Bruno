13e8cc1829cd88a5df6962fcec4327366755afad
Bruno Dedeyne <brunodedeyne@MacBook-Pro-van-Bruno.local>
2018-06-11 13:21:52 +0200

    app added

---
+++ RecipeApp/client/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './components/Main';
+
+import './App.css';
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <div>
+          <Main />
+        </div>
+      );
+  }
+}
---
+++ RecipeApp/client/components/MenuComponents/Menu.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+//Import CSS
+import './Menu.css';
+
+class Menu extends Component {
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Menu;
